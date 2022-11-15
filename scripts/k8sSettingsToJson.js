37607c62f2b7e37362c660079f6b76da60dd44ff
Denis Mishin <dmishin@pomerium.com>
2022-11-14 23:53:08 -0500

    use ingress-controller/docs for k8s reference (#160)

--- scripts/k8sSettingsToJson.js
+++
@@ -1,7 +0,0 @@
-var inputfile = 'src/ingress.pomerium.io_pomerium.yaml',
-  outputfile = 'src/k8sSettings.json',
-  yaml = require('js-yaml'),
-  fs = require('fs'),
-  obj = yaml.load(fs.readFileSync(inputfile, {encoding: 'utf-8'}));
-// this code if you want to save
-fs.writeFileSync(outputfile, JSON.stringify(obj, null, 2));
