7b9f137d4b4cc078f8ed8abbbe4f1e0888bedcb4
zachary painter <60552605+ZPain8464@users.noreply.github.com>
2023-03-06 17:37:40 -0500

    rejects sidebars.js namespacing changes

--- sidebars.js
+++ sidebars.js
@@ -93,15 +93,6 @@ const sidebars = {
           type: 'doc',
           label: 'High Availability',
         },
-<<<<<<< HEAD
-=======
-        {
-          id: 'docs/capabilities/namespacing',
-          className: 'enterprise',
-          type: 'doc',
-          label: 'Namespaces',
-        },
->>>>>>> 6ca95ab (Docs: adds guides index file (#457))
       ],
     },
     {
