7ddf1c67958ae769b5b5892e2a69fe0a43d10ae9
zachary painter <60552605+ZPain8464@users.noreply.github.com>
2023-09-21 10:04:45 -0400

    updates docusaurus and adds gtag to presets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        googleAnalytics: {
+        gtag: {
           trackingID: process.env.GA,
         },
         sitemap: {
