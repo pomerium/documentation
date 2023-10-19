b0c6cfcfe602a97a8456de6c3a58e186a2632934
zachary painter <60552605+ZPain8464@users.noreply.github.com>
2023-10-19 17:20:11 -0400

    adds new videocards component with  mui library

---
+++ src/components/HomepageLayout/VideoCards.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { capitalize } from '@mui/material';
+
+export default function ImgMediaCard() {
+  return (
+    <div>
+      <div>
+      <Card sx={{
+        maxWidth: 850,
+        mb: 7,
+        ml: 0
+        }}>
+        <CardMedia
+          component="iframe"
+          alt="2-minute explainer video of what Pomerium is."
+          height="350"
+          width="100%"
+          src="https://www.youtube.com/embed/WGwC9ULDnAY?rel=0"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Pomerium Demo
+          </Typography>
+          <Typography variant="body1">
+            Learn how Pomerium secures your apps and services in this 2-minute demo.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="text"
+            size="large"
+            onClick={()=>{
+              window.location.href="https://pomerium.com/docs/quickstart"
+            }}
+            >
+              Try Quickstart
+          </Button>
+        </CardActions>
+      </Card>
+      </div>
+      <div>
+      <Card sx={{
+        maxWidth: 850,
+        mb: 7,
+        ml: 0
+        }}>
+        <CardMedia
+          component="iframe"
+          alt="something"
+          height="350"
+          width="100%"
+          src="https://www.youtube.com/embed/bUZUg0e3A1Y"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Clientless Access
+          </Typography>
+          <Typography variant="body1">
+          Learn how Pomerium simplifies access control by providing clientless access to users within your organization.
+          </Typography>
+        </CardContent>
+      </Card>
+      </div>
+      <div>
+      <Card sx={{
+        maxWidth: 850,
+        mb: 7,
+        ml: 0
+        }}>
+        <CardMedia
+          component="iframe"
+          alt="something"
+          height="350"
+          width="100%"
+          src="https://www.youtube.com/embed/3MJrNvQ7aIE"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Continuous Verification
+          </Typography>
+          <Typography variant="body1">
+            Learn what Continuous Verification is, how it works with Pomerium, and why it's important for building a Zero Trust Architecture.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="text"
+            size="large"
+            onClick={()=>{
+              window.location.href="https://www.pomerium.com/docs/capabilities/authorization"
+            }}
+            >
+              See Authorization Docs
+          </Button>
+        </CardActions>
+      </Card>
+      </div>
+    </div>
+  )
+}
+
