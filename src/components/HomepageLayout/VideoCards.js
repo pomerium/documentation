c637270eb300f4037b93e10a25344bbf030955c1
zachary painter <60552605+ZPain8464@users.noreply.github.com>
2023-10-20 12:58:31 -0400

    adds dark mode support

--- src/components/HomepageLayout/VideoCards.js
+++ src/components/HomepageLayout/VideoCards.js
@@ -5,9 +5,12 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Link from '@docusaurus/Link';
+import {useColorMode} from '@docusaurus/theme-common';
 
 
 export default function VideoMediaCard() {
+
+  const {colorMode} = useColorMode();
 
   return (
     <div>
@@ -24,15 +27,35 @@ export default function VideoMediaCard() {
           width="100%"
           src="https://www.youtube.com/embed/WGwC9ULDnAY?rel=0"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent sx={{
+          bgcolor:
+          colorMode === 'dark'
+            ? '#121212'
+            : ""
+        }}>
+          <Typography gutterBottom variant="h5" component="div" sx={{
+              color:
+              colorMode === 'dark'
+                ? '#ebedf0;'
+                : ""
+          }}>
             Pomerium Demo
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" sx={{
+              color:
+              colorMode === 'dark'
+                ? '#ebedf0;'
+                : ""
+          }}>
             Learn how Pomerium secures your apps and services in this 2-minute demo.
           </Typography>
         </CardContent>
-        <CardActions>
+        <CardActions sx={{
+          bgcolor:
+          colorMode === 'dark'
+            ? '#121212'
+            : ""
+        }}>
           <Link to="/docs/quickstart">
             Try Quickstart
           </Link>
@@ -52,11 +75,26 @@ export default function VideoMediaCard() {
           width="100%"
           src="https://www.youtube.com/embed/bUZUg0e3A1Y"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent sx={{
+          bgcolor:
+          colorMode === 'dark'
+            ? '#121212'
+            : ""
+        }}>
+          <Typography gutterBottom variant="h5" component="div" sx={{
+              color:
+              colorMode === 'dark'
+                ? '#ebedf0;'
+                : ""
+          }}>
             Clientless Access
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" sx={{
+              color:
+              colorMode === 'dark'
+                ? '#ebedf0;'
+                : ""
+          }}>
           Learn how Pomerium simplifies access control by providing clientless access to users within your organization.
           </Typography>
         </CardContent>
@@ -75,15 +113,35 @@ export default function VideoMediaCard() {
           width="100%"
           src="https://www.youtube.com/embed/3MJrNvQ7aIE"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent sx={{
+          bgcolor:
+          colorMode === 'dark'
+            ? '#121212'
+            : ""
+        }}>
+          <Typography gutterBottom variant="h5" component="div" sx={{
+              color:
+              colorMode === 'dark'
+                ? '#ebedf0;'
+                : ""
+          }}>
             Continuous Verification
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" sx={{
+              color:
+              colorMode === 'dark'
+                ? '#ebedf0;'
+                : ""
+          }}>
             Learn what Continuous Verification is, how it works with Pomerium, and why it's important for building a Zero Trust Architecture.
           </Typography>
         </CardContent>
-        <CardActions>
+        <CardActions sx={{
+          bgcolor:
+          colorMode === 'dark'
+            ? '#121212'
+            : ""
+        }}>
         <Link to="/docs/capabilities/authorization">
             Go to Authorization Docs
           </Link>
