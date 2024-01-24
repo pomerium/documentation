54d03cbefab6024dee2b3b56c3a0c189527c7af5
zachary painter <60552605+ZPain8464@users.noreply.github.com>
2024-01-24 16:38:03 -0500

    adds styling to component

--- src/theme/Admonition/Types.js
+++ src/theme/Admonition/Types.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+
+import './types.css';
 
 function EnterpriseAdmonition(props) {
   return (
-    <div style={{border: 'solid black 2px', borderRadius: '10px', padding: 10}}>
-      <h5 style={{color: 'blue', fontSize: '95%'}}>{props.title}</h5>
-      <div>{props.children}</div>
+    <div className="custom_admonition">
+      <div className="custom_admonition_header">
+        <span><ArrowCircleUpIcon style={{color: 'rgb(86 83 83)'}}/></span>
+        <h5>{props.title}</h5>
+      </div>
+      <div className="custom_admonition_text">
+        <p>{props.children}</p>
+      </div>
     </div>
   );
 }
