4b3eae7b3c8209c40091af473a2820ffec2a6fff
zachary painter <60552605+ZPain8464@users.noreply.github.com>
2024-01-31 14:28:55 -0500

    Docs: add enterprise admonition (#1198)
    * adds enterprise admonition
    
    * adds inline styling
    
    * adds styling to component
    
    * swaps material ui icon
    
    * adds enterprise admonition examples
    
    * swaps mui icon and deletes gif example
    
    * adds dark mode colors
    
    * updates dark mode palette
    
    * simplify dark mode styling
    
    * updates hex code
    
    ---------
    
    Co-authored-by: Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>

---
+++ src/theme/Admonition/Types.js
@@ -0,0 +1,23 @@
+import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
+import BusinessIcon from '@mui/icons-material/Business';
+
+import styles from './styles.module.css';
+
+function EnterpriseAdmonition(props) {
+  return (
+    <div className={styles.enterprise}>
+      <div className={styles.enterprise_header}>
+        <BusinessIcon />
+        <h5>{props.title}</h5>
+      </div>
+      {props.children}
+    </div>
+  );
+}
+
+const AdmonitionTypes = {
+  ...DefaultAdmonitionTypes,
+  'enterprise': EnterpriseAdmonition,
+}
+
+export default AdmonitionTypes;
