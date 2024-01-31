bc177074d689c1f75b8eee9795e1717bc4af9ce8
zachary painter <60552605+ZPain8464@users.noreply.github.com>
2024-01-30 19:52:17 -0500

    adds dark mode colors

--- src/theme/Admonition/Types.js
+++ src/theme/Admonition/Types.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
 import BusinessIcon from '@mui/icons-material/Business';
+import {useColorMode} from '@docusaurus/theme-common';
 
-import './types.css';
+// import './types.css';
+import styles from './types.module.css';
 
 function EnterpriseAdmonition(props) {
+  const {colorMode} = useColorMode();
+
   return (
-    <div className="custom_admonition">
-      <div className="custom_admonition_header">
-        <span><BusinessIcon style={{color: 'rgb(86 83 83)'}}/></span>
-        <h5>{props.title}</h5>
+    <div className={
+      colorMode === 'dark'
+        ? styles.custom_admonition_dark_mode
+        : styles.custom_admonition_light_mode
+      }
+      >
+      <div className={styles.custom_admonition_header}>
+        <span><BusinessIcon style={
+          colorMode === 'dark'
+            ? {color: '#ECE5FF'}
+            : {color: 'rgb(86 83 83)'}
+          }/></span>
+        <h5 className={
+          colorMode === 'dark'
+            ? styles.custom_admonition_title_dark_mode
+            : styles.custom_admonition_title_light_mode
+          }>{props.title}</h5>
       </div>
-      <div className="custom_admonition_text">
+      <div className={
+        colorMode === 'dark'
+          ? styles.custom_admonition_text_dark_mode
+          : styles.custom_admonition_text
+      }>
         <p>{props.children}</p>
       </div>
     </div>
