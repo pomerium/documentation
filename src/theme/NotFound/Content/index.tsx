ac08d08c97b9f4e5b2a401a0a874e0bbe6765d57
RJ Beers <rjbeers@pomerium.com>
2025-03-07 10:16:50 -0500

    ENG-1873: Add narrative and FAQ to Routes Portal (#1799)

--- src/theme/NotFound/Content/index.tsx
+++ src/theme/NotFound/Content/index.tsx
@@ -6,26 +6,25 @@ import type {WrapperProps} from '@docusaurus/types';
 type Props = WrapperProps<typeof ContentType>;
 
 export default function ContentWrapper(props: Props): JSX.Element {
-
   useEffect(() => {
     if (typeof window !== 'undefined' && window?.gtag) {
       //normal page_view event
-      gtag("event", "page_view", {
-        page_title: "404 Not Found",
+      gtag('event', 'page_view', {
+        page_title: '404 Not Found',
         page_path: window.location.pathname,
-        event_category: "Errors",
-        event_label: "404"
+        event_category: 'Errors',
+        event_label: '404',
       });
 
       //custom event that might be easier to work with
-      window.gtag("event", "not_found", {
-        page_title: "Page Not Found",
+      window.gtag('event', 'not_found', {
+        page_title: 'Page Not Found',
         page_path: window.location.pathname,
-        event_category: "Errors",
-        event_label: "404"
+        event_category: 'Errors',
+        event_label: '404',
       });
     }
-  }, [])
+  }, []);
   return (
     <>
       <Content {...props} />
