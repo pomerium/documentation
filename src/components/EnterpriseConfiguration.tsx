83504816d883c18d62f938947a4c2f90f8d11fe5
Caleb Doxsey <cdoxsey@pomerium.com>
2026-03-04 11:26:22 -0700

    documentation: add components, update terraform tabs (#2099)
    * documentation: add table components, update access log fields
    
    * remove terraform tab, add terraform links
    
    * update to, tls
    
    * add timeouts
    
    * change param name
    
    * wip
    
    * update route settings
    
    * update settings

---
+++ src/components/EnterpriseConfiguration.tsx
@@ -0,0 +1,42 @@
+import React, { PropsWithChildren } from "react";
+
+import { Resource } from "../types";
+
+type EnterpriseConfigurationProps = PropsWithChildren<{
+  name: string;
+  resource: Resource;
+  via?: ("terraform" | "ui")[];
+}>;
+const EnterpriseConfiguration = ({
+  name,
+  resource,
+  via,
+  children,
+}: EnterpriseConfigurationProps) => {
+  via = via || ["terraform", "ui"];
+  const terraformLink = `https://registry.terraform.io/providers/pomerium/pomerium/latest/docs/resources/${resource}#${name}-1`;
+  return (
+    <>
+      {!via.includes("ui") && (
+        <p>
+          The <code>{name}</code> setting is a bootstrap setting and cannot be
+          configured in the Console UI.{" "}
+          {via.includes("terraform") && (
+            <>
+              However it can be configured by{" "}
+              <a href={terraformLink}>Terraform</a>.
+            </>
+          )}
+        </p>
+      )}
+      {children}
+      {via.includes("ui") && via.includes("terraform") && (
+        <p>
+          The <code>{name}</code> setting can also be configured by{" "}
+          <a href={terraformLink}>Terraform</a>.
+        </p>
+      )}
+    </>
+  );
+};
+export default EnterpriseConfiguration;
